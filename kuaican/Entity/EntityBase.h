9d28c7933ff9197fd3df283bd456c8c626e41243
levinkkk <levinkkk@126.com>
2013-03-12 21:21:49 +0800

    commint

---
+++ kuaican/Entity/EntityBase.h
@@ -0,0 +1,13 @@
+//
+//  EntityBase.h
+//  kuaican
+//
+//  Created by 林成坚 on 13-3-12.
+//  Copyright (c) 2013年 林成坚. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EntityBase : NSObject
+
+@end
---
+++ kuaican/Entity/EntityBase.m
@@ -0,0 +1,13 @@
+//
+//  EntityBase.m
+//  kuaican
+//
+//  Created by 林成坚 on 13-3-12.
+//  Copyright (c) 2013年 林成坚. All rights reserved.
+//
+
+#import "EntityBase.h"
+
+@implementation EntityBase
+
+@end
